c875bfe55a2f3119d62646c86d436e4c0999eb67
Danel <mantillapalominodanel@gmail.com>
2025-12-14 12:13:36 -0500

    separando respuestas de las apis

--- app/api/danel/route.js
+++ app/api/danel/route.js
@@ -14,25 +14,25 @@ export async function GET(request){
         
         //VERIFICANDO EXISTENCIA DEL AUTH
         if(!authHeader){
-            return NextResponse.json({status: 403, message: "no autorizado, api-key faltante"})
+            return NextResponse.json({status: 403},{message: "no autorizado, api-key faltante"})
         }
         
         //VERFICANDO EL FORMATO DEL AUTH
         if(!authHeader.startsWith("Bearer ")){
-            return NextResponse.json({status:403, message: "formato de autorizacion invalido"})
+            return NextResponse.json({status:403},{message: "formato de autorizacion invalido"})
         }
         const token = authHeader.split(" ")[1];
         
         //VERIFICANDO VALIDEZ DEL TOKEN
         if(token !== process.env.API_MOTT){
-            return NextResponse.json({status: 403, message: "token invalido"})
+            return NextResponse.json({status: 403},{message: "token invalido"})
         }
         
-        return NextResponse.json({status:200, users: db})
+        return NextResponse.json({status:200},{users: db})
     }
     catch(e){
         console.error("a ocurrido un error en el servidor");
-        return NextResponse.json({status: 500, message: "a ocurrido un error en el server"})
+        return NextResponse.json({status: 500},{message: "a ocurrido un error en el server"})
     }
 }
 
@@ -46,36 +46,36 @@ export async function POST(request){
         
         //VERIFICANDO EXISTENCIA DEL AUTH
         if(!authHeader){
-            return NextResponse.json({status: 403, message: "no autorizado, api-key faltante"})
+            return NextResponse.json({status: 403},{message: "no autorizado, api-key faltante"})
         }
         //VERIFICANDO EL FORMATO DEL AUTH
         if(!authHeader.startsWith("Bearer ")){
-            return NextResponse.json({status: 403, message: "formato de autorizacion invalido"})
+            return NextResponse.json({status: 403},{message: "formato de autorizacion invalido"})
         }
         const token = authHeader.split(" ")[1];
         
         //VERIFICANDO VALIDEZ DEL TOKEN
         if(token !== process.env.API_MOTT){
-            return NextResponse.json({status: 403, message: "token invalido"})
+            return NextResponse.json({status: 403},{message: "token invalido"})
         }
         
         //PETICION DEL USUARIO
         userPeticion = await request.json();
         
         if(!userPeticion){
-            return NextResponse.json({status: 400, message: "faltan datos en el cuerpo de la peticion"})
+            return NextResponse.json({status: 400},{message: "faltan datos en el cuerpo de la peticion"})
         }
         if(!userPeticion.id || !userPeticion.nombre || !userPeticion.edad || !userPeticion.cargo){
-            return NextResponse.json({status: 400, message: "faltan datos requeridos en el usuario a agregar"})
+            return NextResponse.json({status: 400},{message: "faltan datos requeridos en el usuario a agregar"})
         }
         
         db.push(userPeticion);
-        return NextResponse.json({status: 200, message: "user agregado correctamente", users: db});
+        return NextResponse.json({status: 200},{message: "user agregado correctamente", users: db});
     
     }
     catch(e){
         console.log(e.message||"error en el servidor");
-        return NextResponse.json({status: 500, message:"error interno del servidor"})
+        return NextResponse.json({status: 500},{message:"error interno del servidor"})
     }
 }
 
@@ -87,51 +87,51 @@ export async function PUT(request){
         const authHeader = request.headers.get("authorization")
         //VERFICANDO EXISTENCIA DEL AUTH
         if(!authHeader){
-            return NextResponse.json({status: 403, message:"No autorizado, api-key faltante"})
+            return NextResponse.json({status: 403},{message:"No autorizado, api-key faltante"})
         }
         
         //VERIFICANDO EL FORMATO DEL AUTH
         if(!authHeader.startsWith("Bearer ")){
-            return NextResponse.json({status:403, message: "formato de autorizacion invalido"})
+            return NextResponse.json({status:403},{message: "formato de autorizacion invalido"})
         }
         
         const token = authHeader.split(" ")[1];
         
         //VERIFICANDO VALIDEZ DEL TOKEN
         if(token !== process.env.API_MOTT){
-            return NextResponse.json({status: 403, message: "token invalido"})
+            return NextResponse.json({status: 403},{message: "token invalido"})
         }
         
         //PETICION DEL USUARIO
         userPeticion = await request.json();
         
         if(!userPeticion){
-            return NextResponse.json({status: 400, message: "faltan datos en el cuerpo de la peticion"});
+            return NextResponse.json({status: 400},{message: "faltan datos en el cuerpo de la peticion"});
         }
         else if(!userPeticion.ID){
-            return NextResponse.json({status: 400, message: "¡Falta el ID del elemento a cambiar !"});
+            return NextResponse.json({status: 400},{message: "¡Falta el ID del elemento a cambiar !"});
         }
         else if(!userPeticion.Changes){
-            return NextResponse.json({status: 400, message: "¡Faltan los cambios para el ID selecionado"});
+            return NextResponse.json({status: 400},{message: "¡Faltan los cambios para el ID selecionado"});
         }
         else if(!userPeticion.Changes.id){
-            return NextResponse.json({status: 400, message: "ingresa el nuevo ID"});
+            return NextResponse.json({status: 400},{message: "ingresa el nuevo ID"});
         }
         else if(!userPeticion.Changes.nombre){
-            return NextResponse.json({status: 400, message: "ingresa el nuevo nombre"});
+            return NextResponse.json({status: 400},{message: "ingresa el nuevo nombre"});
         }
         else if(!userPeticion.Changes.edad){
-            return NextResponse.json({status: 400, message: "ingresa la nueva edad"});
+            return NextResponse.json({status: 400},{message: "ingresa la nueva edad"});
         }
         else if(!userPeticion.Changes.cargo){
-            return NextResponse.json({status: 400, message: "ingresa el nuevo cargo"})
+            return NextResponse.json({status: 400},{message: "ingresa el nuevo cargo"})
         }
         
         //buscando objeto en la base de datos
         const userEdit  = db.findIndex(user => user.id == userPeticion.ID);
         
         if(userEdit == -1){
-            return NextResponse.json({status: 400, message: `el usuario con el ID ${userPeticion.ID} no existe`})
+            return NextResponse.json({status: 400},{message: `el usuario con el ID ${userPeticion.ID} no existe`})
         }
         
         //cambiando datos en la base de datos
@@ -140,12 +140,12 @@ export async function PUT(request){
         db[userEdit].edad = userPeticion.Changes.edad;
         db[userEdit].cargo = userPeticion.Changes.cargo;
         
-        return NextResponse.json({status: 201, message: "usuario actualizado correctamente", users: db})
+        return NextResponse.json({status: 201},{message: "usuario actualizado correctamente", users: db})
         
     }
     catch(e){
         console.error(e);
-        return NextResponse.json({status: 500, message: "error en el servidor al editar un usuario"})
+        return NextResponse.json({status: 500},{message: "error en el servidor al editar un usuario"})
     }
 }
 
@@ -159,44 +159,44 @@ export async function DELETE(request) {
         
         //VERIFICANDO EXISTENCIA DEL AUTH
         if(!authHeader){
-            return NextResponse.json({status: 403, message: "no autorizado, api-key faltante"})
+            return NextResponse.json({status: 403},{message: "no autorizado, api-key faltante"})
         }
         //VERIFICANDO EL FORMATO DEL AUTH
         if(!authHeader.startsWith("Bearer ")){
-            return NextResponse.json({status: 403, message: "formato de autorizacion invalido"})
+            return NextResponse.json({status: 403},{message: "formato de autorizacion invalido"})
         }
         
         const token = authHeader.split(" ")[1];
         
         //VERIFICANDO VALIDEZ DEL TOKEN
         if(token !== process.env.API_MOTT){
-            return NextResponse.json({status: 403, message: "token invalido" })
+            return NextResponse.json({status: 403},{message: "token invalido" })
         } 
-        
+
         //PETICION DEL USUARIO
         userPeticion = await request.json();
         if(!userPeticion){
-            return NextResponse.json({status: 400, message: "faltan datos en el cuerpo de la peticion"})
+            return NextResponse.json({status: 400},{message: "faltan datos en el cuerpo de la peticion"})
         }
         
         if(!userPeticion.ID){
-            return NextResponse.json({state: 400, message: "ingresa un ID para eliminar un elemento"})
+            return NextResponse.json({state: 400},{message: "ingresa un ID para eliminar un elemento"})
         }
         
         const IdDelete = db.findIndex(users => users.id === userPeticion.ID);
         
         if(IdDelete == -1){
-            return NextResponse.json({state: 400, message: "el ID no existe en la base de datos"});
+            return NextResponse.json({state: 400},{message: "el ID no existe en la base de datos"});
         }
         //ELIMINANDO ELEMENTO DE LA BASE DE DATOS
         db.splice(IdDelete, 1);
         
-        return NextResponse.json({state: 200, message: "elemento eliminado correctamente", users: db});
+        return NextResponse.json({state: 200},{message: "elemento eliminado correctamente", users: db});
         
         
     }
     catch(e){
         console.error(e);
-        return NextResponse.json({status: 500, message: "error del servidor al eliminar a un usuario"})
+        return NextResponse.json({status: 500},{message: "error del servidor al eliminar a un usuario"})
     }
 }
